be8cc2494e37b650d42c00b7a69cf8ae6c39d300
mariaalvarez11 <70607300+mariaalvarez11@users.noreply.github.com>
2021-08-21 15:37:13 -0400

    Add files via upload

---
+++ colorPicker.py
@@ -0,0 +1,15 @@
+import random
+
+print("rgb color picker")
+
+import random
+r = random.randint(0,255)
+g = random.randint(0,255)
+b = random.randint(0,255)
+rgb = [r,g,b]
+
+
+input("ask a question: ")
+print(rgb)
+
+print("to get the color, copy the numbers given in an online color picker!")
